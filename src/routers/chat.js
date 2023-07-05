b57030e42f17deb14e39c945df3473a8184864a8
Derryl <derryl.taufik@traveloka.com>
2023-07-05 20:50:21 +0700

    fix hotels return data

--- src/routers/chat.js
+++ src/routers/chat.js
@@ -32,10 +32,12 @@ router.post("/chat/completions", async (req, res) => {
     var hotels = reply.substring(jsonStartIndex, jsonEndIndex);
 
     // Trailing message is needed because sometimes the chatgpt unexpectedly write something after hotel recommendations
-    
+
     var trailing_message = reply.substring(jsonEndIndex);
 
-    return res.status(200).send({ data: { message, hotels, trailing_message } });
+    return res
+      .status(200)
+      .send({ data: { message, hotels, trailing_message } });
   } catch (error) {
     console.log(error);
     return res
@@ -48,22 +50,22 @@ router.post("/mock/chat/completions", async (req, res) => {
   try {
     const message =
       'Great! Here\'s a suggested itinerary for your 7-day trip to Bandung:\n\nDay 1:\n- Arrive at Husein Sastranegara International Airport\n- Check-in at your hotel\n- Visit Tangkuban Perahu volcano to enjoy the scenic beauty of the active volcano\n- Try traditional Sundanese food for lunch at "Kampung Daun" restaurant\n- Visit Dago Pakar area for the amazing view of Bandung city from the hills\n\nDay 2:\n- Visit Kawah Putih, an impressive white crater lake located in Ciwidey area\n- Have a lunch at floating market in Lembang area\n- Visit the famous tea plantations in Ciwidey area\n- Back to hotel\n\nDay 3:\n- Visit the beautiful Maribaya waterfall and hot springs\n- Explore the amazing trees at Treetop Adventure Park\n- Have a lunch at a local restaurant near the area\n- Visit the stunning Glamping Lakeside in Situ Cileunca\n\nDay 4:\n- Visit the Tangkuban Parahu volcano again, but this time take a different route through Lembang\n- Have a lunch at "The Peak" restaurant with a panoramic view of Bandung city\n- Visit the Instagenic European-style Castle, "Gedung Sate"\n\nDay 5:\n- Visit the amazing Trans Studio Bandung, a theme park with indoor and outdoor rides\n- Have lunch at the theme park\'s restaurants\n- Visit the Paris Van Java shopping mall for a shopping spree\n\nDay 6:\n- Relax at the Jendela Alam Farm in Lembang and take photos with the farm animals\n- Have lunch at a local restaurant near the area\n- Visit the Rabbit town, a park with various types of rabbits\n\nDay 7:\n- Visit the Cihampelas Walk shopping street, famous for its jeans and fashion shops\n- Have lunch at a local restaurant near the area\n- Check-out from your hotel and head to the airport for your flight back home\n\nRecommended hotels:\n';
-    const hotels = {
-      recommendedHotels: [
-        {
-          city: "Bandung",
-          hotels: [
-            "Padma Hotel Bandung",
-            "The Trans Luxury Hotel Bandung",
-            "Aryaduta Bandung",
-          ],
-        },
-      ],
-    };
+    const hotels = [
+      {
+        city: "Bandung",
+        hotels: [
+          "Padma Hotel Bandung",
+          "The Trans Luxury Hotel Bandung",
+          "Aryaduta Bandung",
+        ],
+      },
+    ];
 
-    const trailing_message= "I hope you enjoy your trip!"
+    const trailing_message = "I hope you enjoy your trip!";
 
-    return res.status(200).send({ data: { message, hotels, trailing_message } });
+    return res
+      .status(200)
+      .send({ data: { message, hotels, trailing_message } });
   } catch (error) {
     console.log(error);
     return res
